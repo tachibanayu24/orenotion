c79e61a123cdd407fb76a094ccf68342973a07c6
Yuto Tachibana <yuto.tachibana@dena.com>
2023-03-02 15:29:26 +0900

    tier

--- src/models/page/page.ts
+++ src/models/page/page.ts
@@ -2,17 +2,30 @@ import { JSONContent } from '@tiptap/core'
 
 import { Entity } from '../__common__/entity'
 
+type PageClass =
+  // 管理者のみが閲覧・編集できる
+  | 'tier1'
+  // 管理者が閲覧・編集できる / トークンを所持している一般ユーザーが閲覧できる
+  | 'tier2'
+  //  管理者のみが閲覧・編集できる / 一般ユーザーが閲覧できる
+  | 'tier3'
+  // すべてのユーザーが閲覧・編集できる
+  | 'tier4'
+
 export class Page extends Entity {
   emoji: string
   title: string
-
+  pageClass: PageClass
   content?: JSONContent
+  publishedAt?: Date
 
   constructor(init: Page) {
     super(init)
 
     this.emoji = init.emoji
     this.title = init.title
+    this.pageClass = init.pageClass
     this.content = init.content
+    this.publishedAt = init.publishedAt
   }
 }
